9d57462f080f60d7f96ad0afa5a391b5dd10f40e
Xavier Arias Botargues <xavier1923@gmail.com>
2017-11-21 23:52:01 +0100

    Added NearestNeighborPictureBox to support showing images without filtering on a PictureBox

---
+++ src/LibDayDataExtractor/Forms/NearestNeighborPictureBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace LibDayDataExtractor.Forms
+{
+    public partial class NearestNeighborPictureBox : PictureBox
+    {
+        public NearestNeighborPictureBox()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs eventArgs)
+        {
+            eventArgs.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+            base.OnPaint(eventArgs);
+        }
+    }
+}
